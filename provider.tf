8f06433338098f77709a4e4505f565080b33ec9b
Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>
2024-11-21 15:23:35 +0530

    Update provider.tf
    Signed-off-by: Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "ap-south-1"  # Change to your AWS region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "path/to/my/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "my-terraform-lock-table"
+  }
+}
